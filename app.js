7d021e17c0ab1aa88b94adfd48a69c68ffc3525b
Benjamin Ling <60065083+iineb@users.noreply.github.com>
2020-02-16 20:26:08 +0000

    UPDATE: Basic Styling and Functionality added

--- app.js
+++ app.js
@@ -1,29 +1,76 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
-
+const mongoose = require("mongoose");
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const newList = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema = {
+	task: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const itemOne = new Item({
+	task: "Default Task One"
+});
+
+const itemTwo = new Item({
+	task: "Default Task Two"
+});
+
+const itemThree = new Item({
+	task: "Default Task Three"
+});
+
+const defaultItems = [itemOne, itemTwo, itemThree];
 
 app.get("/", function(req, res) {
 	const day = date.getDate();
-	res.render("list", {listTitle: day, newListItems: newList});
+
+	Item.find({}, function(err, foundItems) {
+		if (err) {
+			console.log(err);
+		} else if (foundItems.length === 0){
+			Item.insertMany(defaultItems, function(err) {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log("Successfully added default items to the database");
+				}
+			});
+			res.redirect("/");
+		} else {
+			res.render("list", {listTitle: day, newListItems: foundItems})
+		}
+	});
+	
 });
 
 app.post("/", function(req, res){
 	const newItem = req.body.newTask;
-    newList.push(newItem);
-    res.redirect("/");
+	const item = new Item({
+		task: newItem
+	});
+	item.save();
+	res.redirect("/");
 });
 
-app.get("/add-a-task", function(req,res){
-    res.render("list", {listTitle: "Add a task to do", newListItems: newList});
+app.post("/delete", function(req, res) {
+	const id = req.body.deleteTask;
+	Item.findByIdAndRemove(id, function(err) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log("Task deleted successfully.");
+		}
+	});
+	res.redirect("/");
 });
 
 app.listen(3000, function() {
