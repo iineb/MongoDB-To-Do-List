13649cf307d61826a4410567c38e912b0b162035
Benjamin Ling <60065083+iineb@users.noreply.github.com>
2020-02-18 19:37:25 +0000

    UPDATE: Quality of life improvements

--- app.js
+++ app.js
@@ -17,36 +17,38 @@ const itemsSchema = {
 const Item = mongoose.model("Item", itemsSchema);
 
 const itemOne = new Item({
-	task: "Default Task One"
+	task: "You have no tasks today!"
 });
 
-const itemTwo = new Item({
-	task: "Default Task Two"
-});
+const defaultItems = [itemOne];
 
-const itemThree = new Item({
-	task: "Default Task Three"
-});
+const listSchema = {
+	name: String,
+	items: [
+		itemsSchema
+	]
+};
 
-const defaultItems = [itemOne, itemTwo, itemThree];
+const List = mongoose.model("List", listSchema);
 
 app.get("/", function(req, res) {
 	const day = date.getDate();
-
 	Item.find({}, function(err, foundItems) {
 		if (err) {
 			console.log(err);
 		} else if (foundItems.length === 0){
-			Item.insertMany(defaultItems, function(err) {
-				if (err) {
-					console.log(err);
-				} else {
-					console.log("Successfully added default items to the database");
-				}
-			});
-			res.redirect("/");
+			// Item.insertMany(defaultItems, function(err) {
+			// 	if (err) {
+			// 		console.log(err);
+			// 	} else {
+			// 		console.log("Successfully added default items to the database");
+			// 	}
+			// });
+			const emptyTasks = "You currently have no tasks today!";
+			res.render("list", {listTitle: day, newListItems: foundItems, notifications: emptyTasks});
 		} else {
-			res.render("list", {listTitle: day, newListItems: foundItems})
+			const fullTasks = "Your tasks for today are:";
+			res.render("list", {listTitle: day, newListItems: foundItems, notifications: fullTasks});
 		}
 	});
 	
@@ -64,14 +66,31 @@ app.post("/", function(req, res){
 app.post("/delete", function(req, res) {
 	const id = req.body.deleteTask;
 	Item.findByIdAndRemove(id, function(err) {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log("Task deleted successfully.");
-		}
+		err ? console.log(err) : console.log("Task deleted successfully.");;
 	});
 	res.redirect("/");
 });
+
+// app.get("/:customList", function(req, res) {
+// 	const id = req.params.customList;
+	
+// 	List.findOne({name: id}, function(err, foundList) {
+// 		if (err) {
+// 			console.log(err);
+// 		} else {
+// 			if (!foundList) {
+// 				const list = new List({
+// 					name: id,
+// 					items: defaultItems
+// 				});
+// 				list.save();
+// 				res.redirect(`/${id}`);
+// 			} else {
+// 				res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+// 			}
+// 		}
+// 	});
+// });
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
